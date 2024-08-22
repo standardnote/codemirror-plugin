df62623d525805f286e688ec3ed84c4abe8760e1
ixiumu <ixiumu@users.noreply.github.com>
2024-08-22 19:16:43 +0000

    mobile

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import EditorKit, { EditorKitDelegate } from '@standardnotes/editor-kit';
 import MarkdownEditor from '@uiw/react-markdown-editor';
-import { Commands } from '@uiw/react-markdown-editor/cjs/components/ToolBar'
+import { Commands } from '@uiw/react-markdown-editor/cjs/components/ToolBar';
 import type { OutgoingItemMessagePayload } from '@standardnotes/snjs';
 
 export interface EditorInterface {
   preview: boolean;
   text: string;
+  colorMode: string;
 }
 
 const initialState = {
   preview: false,
   text: '',
+  colorMode: 'light',
 };
 
 // let keyMap = new Map();
@@ -33,36 +35,38 @@ export default class Editor extends React.Component<{}, EditorInterface> {
       /** This loads every time a different note is loaded */
       onNoteValueChange: async (note: OutgoingItemMessagePayload) => {
         this.setState({
-          preview: this.editorKit?.getComponentDataValueForKey('mode') || false
-        })
-      },
-      setEditorRawText: (text: string) => {
-        this.setState({
-          text: text,
+          preview: this.editorKit?.getComponentDataValueForKey('mode') || false,
         });
       },
-      clearUndoHistory: () => { },
+      setEditorRawText: (text: string) => {
+        this.setState({ text: text });
+      },
+      clearUndoHistory: () => {},
       handleRequestForContentHeight: () => undefined,
       onThemesChange: () => {
-        const themes = this.editorKit?.componentRelay?.component.activeThemes || []
-        if (themes.length) {
-          console.log(themes)
-          const isDark = [
-            'dark',
-            'org.standardnotes.theme-focus',
-            'com.standardnotes.theme-proton',
-            'org.standardnotes.theme-futura',
-            'org.standardnotes.theme-midnight',
-            'org.standardnotes.theme-github',
-          ].some((theme: string) => themes[0].includes(theme.toLowerCase()))
-          document.documentElement.setAttribute('data-color-mode', isDark ? 'dark' : 'light')
-        }
+        this.setColorMode(
+          this.editorKit?.componentRelay?.component.activeThemes || [],
+        );
       },
     };
 
     this.editorKit = new EditorKit(delegate, {
       mode: 'plaintext',
     });
+  };
+
+  setColorMode = (themes: string) => {
+    if (themes.length) {
+      const isDark = [
+        'dark',
+        'org.standardnotes.theme-focus',
+        'com.standardnotes.theme-proton',
+        'org.standardnotes.theme-futura',
+        'org.standardnotes.theme-midnight',
+        'org.standardnotes.theme-github',
+      ].some((theme: string) => themes[0].includes(theme.toLowerCase()));
+      this.setState({ colorMode: isDark ? 'dark' : 'light' });
+    }
   };
 
   saveText = (text: string) => {
@@ -84,45 +88,51 @@ export default class Editor extends React.Component<{}, EditorInterface> {
     }
   };
 
-  onFocus = (e: React.FocusEvent) => 
-    this.editorKit?.componentRelay?.sendCustomEvent('focus', {})
+  onFocus = (e: React.FocusEvent) =>
+    this.editorKit?.componentRelay?.sendCustomEvent('focus', {});
 
   onBlur = (e: React.FocusEvent) =>
-    this.editorKit?.componentRelay?.sendCustomEvent('blur', {})
+    this.editorKit?.componentRelay?.sendCustomEvent('blur', {});
 
   onPreviewMode = (isPreview: boolean) => {
-    this.editorKit?.setComponentDataValueForKey('mode', isPreview)
-  }
+    this.editorKit?.setComponentDataValueForKey('mode', isPreview);
+  };
 
-  //   onKeyDown = (e: React.KeyboardEvent | KeyboardEvent) => {
-  //     keyMap.set(e.key, true);
-  //     // Do nothing if 'Control' and 's' are pressed
-  //     if (keyMap.get('Control') && keyMap.get('s')) {
-  //       e.preventDefault();
-  //     }
-  //   };
+  isMobile: boolean = window.innerWidth < 768;
 
-  //   onKeyUp = (e: React.KeyboardEvent | KeyboardEvent) => {
-  //     keyMap.delete(e.key);
-  //   };
-
-  isMobile: boolean = window.innerWidth < 768
-
-  toolbars: Commands[] = this.isMobile ? ['undo', 'redo', 'bold', 'italic'] :
-    ['undo', 'redo', 'bold', 'italic', 'header', 'strike', 'underline', 'code', 'quote', 'olist', 'ulist', 'todo', 'link', 'image']
+  toolbars: Commands[] = this.isMobile
+    ? ['undo', 'redo', 'bold', 'italic']
+    : [
+        'undo',
+        'redo',
+        'bold',
+        'italic',
+        'header',
+        'strike',
+        'underline',
+        'code',
+        'quote',
+        'olist',
+        'ulist',
+        'todo',
+        'link',
+        'image',
+      ];
 
   render() {
-    const { text, preview } = this.state;
+    const { text, preview, colorMode } = this.state;
     return (
-      <MarkdownEditor
-        value={text}
-        onBlur={this.onBlur}
-        onFocus={this.onFocus}
-        visible={preview}
-        toolbars={this.toolbars}
-        onPreviewMode={this.onPreviewMode}
-        onChange={(value, viewUpdate) => this.saveText(value)}
-      />
+      <div className="sn-editor" data-color-mode={colorMode}>
+        <MarkdownEditor
+          value={text}
+          onBlur={this.onBlur}
+          onFocus={this.onFocus}
+          visible={preview}
+          toolbars={this.toolbars}
+          onPreviewMode={this.onPreviewMode}
+          onChange={(value, viewUpdate) => this.saveText(value)}
+        />
+      </div>
     );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 import Editor from './components/Editor';
 import './stylesheets/main.scss';
 
-const container = document.getElementById('root');
-
-const root = createRoot(container!);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Editor />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
